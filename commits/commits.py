cb52ac07ce028473620b0baab68eb1734fe672cb
life0nmars <43827050+life0nmars@users.noreply.github.com>
2021-03-07 19:41:30 +0300

    REQUESTS-1013 Added commits.py

--- commits/commits.py
+++ commits/commits.py
@@ -1,16 +1,66 @@
-TOKEN = str(input())
-
 import requests
+import json
+
+
+PREFIX = ['LEETCODE', 'GENERATOR', 'TRIANGLE', 'HEXNUMBER', 'REQUESTS']
+GROUP = '1013'
+ACTION = ['Added', 'Deleted']
+TOKEN = ''
+
 
 headers = {'Authorization': 'token ' + TOKEN}
 
-login = requests.get('https://api.github.com/user', headers=headers)
-print(login.json())
-def get_headers():
-    return {'Authorization': 'token ' + TOKEN}
-def get_user_pr(userName = 'life0nmars', repos = 'python_au', state = 'open'):
-  r = requests.get("https://api.github.com/repos/{}/{}/pulls?state={}".format(userName, repos, state), headers = get_headers())
-  for elem in r.json():
-    commits = requests.get(elem["commits_url"], headers = get_headers()).json()
-    if len(commits) > 1:
-      return (len(commits))
+def get_all_user_pr(userName = "life0nmars", repos = 'python_au', state = 'open'):
+    all_prs = requests.get("https://api.github.com/repos/{}/{}/pulls?state={}".format(userName, repos, state), headers = headers).json()
+    return all_prs
+
+def get_all_pr_commits(pr):
+    list_of_commits = requests.get(pr['commits_url'], headers = headers).json()
+    return list_of_commits
+
+def get_title(dictionary):
+    return dictionary['commit']['message']
+
+def check_prefixes(title):
+    title = title.replace('-', ' ')
+    parsed_title = title.split(' ')
+    errors = list()
+    if len(parsed_title) < 4:
+        return ('Comment is incorrect')
+    if parsed_title[0] not in PREFIX:
+        errors.append('Wrong: {}, Right: {}.'.format(parsed_title[0], PREFIX))
+    if parsed_title[1] not in GROUP:
+        errors.append('Wrong: {} Right: {}.'.format(parsed_title[1], GROUP))
+    if parsed_title[2] not in ACTION:
+        errors.append('Wrong:{} Right: {}.'.format(parsed_title[2], ACTION))
+    if len(errors) == 0:
+      return 'Right'
+    else:
+      title_errors = '\n'.join(errors)
+    return title_errors
+
+def create_pr_comment(title, title_errors):
+    return('Hello! \n You should have been more attentive: \n {} \n Mistakes: \n {} '.format(title, title_errors))
+
+def send_pr_comment(pr, comment):
+    data = {'body': comment,
+            'path': requests.get(pr['url']+'/files', headers=headers).json()[0]['filename'],
+            'position': 1,
+            'commit_id': pr['head']['sha']}
+    r = requests.post(pr['url']+'/comments', headers=headers, json=data)
+    print(r.json())
+
+def bully_your_pull_request(pr):
+    commits = get_all_pr_commits(pr)
+    for commit in commits:
+        title = get_title(commit)
+        title_errors = check_prefixes(title)
+        comment = create_pr_comment(title, title_errors)
+        send_pr_comment(pr, comment)
+
+def main():
+    for pr in get_all_user_pr(userName = 'life0nmars'):
+      bully_your_pull_request(pr)
+
+if __name__ == '__main__':
+    main()
