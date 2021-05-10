e3799a9dfd512cf402f2238b8a68b733384a906c
life0nmars <43827050+life0nmars@users.noreply.github.com>
2021-05-10 14:24:27 +0300

    GRAPH-1013 Updated data_analysis.py

--- graph/data_analysis.py
+++ graph/data_analysis.py
@@ -1,65 +1,55 @@
 from datetime import datetime
 import matplotlib as plt
-import sys
-
-
-def open_file(file_name='input.txt'):
+import matplotlib.pyplot as plt
+def open_file(file_name):
     f = open(file_name)
     data = f.readlines()
     f.close()
     return data
 
+def prepearing_of_data(data):
+    keys = data.pop(0).strip("\n").split(", ")
+    sorted_data = []
+    for elem in data:
+       sorted_data.append(elem.rstrip("\n").split(", "))
+    return sorted_data, keys
 
-def create_list_of_dicts(data):
-    headers = data.pop(0).strip(' ').strip('\n').split(',')
-    data_str_dic = []
-    for data_row in data:
-        data_str_dic.append(dict(zip(headers, data_row.split(','))))
-    return data_str_dic
+def make_the_dict(keys, sorted_data):
+    lordOfDicts = []
+    for lst in sorted_data:
+      lordOfDicts.append(dict(zip(keys, lst)))
+    return lordOfDicts
 
-
-def filter_data(data, key, value):
-    return list(filter(lambda elem: elem[key] == value, data))
-
+def filter_dicts(lordOfDicts, key, value):
+    return list(filter(lambda elem: elem[key] == value, lordOfDicts))
 
 def sort_data(data, sort_key):
-    return sorted(data, key=lambda k: datetime.strptime(k[sort_key], '%Y-%M'))
+    return sorted(data, key=lambda k: datetime.strptime(k[sort_key], '%d-%m-%Y'))
 
-
-def prepare_data(data, data_filter=None):
-    if data_filter is not None:
-        for one_data_filter in data_filter.split(','):
-            key = one_data_filter.split('=')[0]
-            value = one_data_filter.split('=')[1]
-            data = filter_data(data, key, value)
-    return data
-
-
-def make_plot(data, ordinate_key, abscissa_key='date', data_filter=''):
-    ordinate = []
-    abscissa = []
-    for data_row in data:
-        ordinate.append(data_row[ordinate_key])
-        abscissa.append(data_row[abscissa_key])
-    if abscissa_key == 'date':
-        abscissa = list(map(lambda x: str(x)[0:7], abscissa))
-
-    plt.title('{}({})'.format(ordinate_key, abscissa_key))
-    plt.xlabel(abscissa_key)
-    plt.ylabel(ordinate_key)
-    plt.plot(abscissa, ordinate)
-    plt.savefig('plot {}({}): {}.png'.format(ordinate_key, abscissa_key, data_filter))
+def get_x_and_y(data, title, key_x = "Date", key_y = "Staff"):
+    x = []
+    y = []
+    for i in data:
+        x.append(i[key_x])
+        y.append(int(i[key_y]))
+    plt.plot(x, y,'b', x, y,"y*")
+    plt.title(title)
+    plt.grid(True)
+    plt.ylim([min(y) - 1,max(y) + 1])
+    plt.xlabel(key_x)
+    plt.ylabel(key_y)
     plt.show()
 
-
-def main(file_name):
-    data_str = open_file(file_name)
-    data = create_list_of_dicts(data_str)
-    data = prepare_data(data, sys.argv[3])
-    make_plot(data, sys.argv[1], sys.argv[2])
-
-
-# To run program you should write '....txt' 'ordinate_key' 'abscissa_key' 'data_filter'
-# Format of data_filter is key1=value1,key2=value2,...
+def main(name):
+    data = open_file(name)
+    data, headers = prepearing_of_data(data)
+    lstOfDicts = make_the_dict(headers, data)
+    title = input("Number of the resourse:")
+    dictionary = filter_dicts(lstOfDicts, headers[0], title)
+    data = sort_data(dictionary, headers[1])
+    print(headers)
+    get_x_and_y(data, title, headers[1], headers[2])
+#Подразумевается, что в файле сначала пишется ресурс, потом дата, потом количество произведенных товаров.
+#Если порядок не такой, надо будет изменить индексы списка headers.
 if __name__ == '__main__':
     main('input.txt')
