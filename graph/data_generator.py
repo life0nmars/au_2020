bf79ae1a3f480e4e33a82fd68dd943c2537aaf63
life0nmars <43827050+life0nmars@users.noreply.github.com>
2021-05-16 13:43:14 +0300

    GRAPH-1013 Updated data_generator.py

--- graph/data_generator.py
+++ graph/data_generator.py
@@ -1,39 +1,60 @@
 import random
+import sys
 
-headers = 'Resourse, Date, Stuff\n'
-month = {'01': 31, '02': 28, '03': 31, '04': 30, '05': 31, '06': 30, '07': 31, '08': 31, '09': 30, '10': 31, '11': 30,
-         '12': 31}
+headers = ['Resourse','Date','Staff','Stuff\n']
+staff = ["Anya","Annushka", "Anna", "Anuta", "Anechka"]
+
 class Randomizer:
     def __init__(self, resourse):
         self.resourse = resourse
-        self.month = month
+        self.staff = staff
+
+
+    def dictOfMonths(self):
+        day_31 = dict.fromkeys(['01','03','05','07','08','10','12'],31)
+        day_30 = dict.fromkeys(['04','06','09','11'],30)
+        month = {'02':28}
+        month.update(day_31)
+        month.update(day_30)
+        return month
+
 
     def random_date(self):
         lstmonth = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
         rmonth = random.choice(lstmonth)
-        amountOfDays = self.month[rmonth]
+        month = self.dictOfMonths()
+        amountOfDays = month[rmonth]
         days = []
         for i in range(1, amountOfDays + 1):
             days.append(i)
             i += 1
         return '{}-{}-{}'.format(random.choice(days), rmonth, 2020)
 
+
     def random_resourse(self):
         return '{}'.format(random.choice(self.resourse))
+
 
     def random_stuff(self):
         return '{}'.format(random.randint(80, 130))
 
+
+    def random_staff(self):
+        return '{}'.format(random.choice(self.staff))
+
+
     def all_in(self):
-        return '{}, {}, {}'.format(self.random_resourse(), self.random_date(), self.random_stuff())
+        return '{}, {}, {}, {}'.format(self.random_resourse(), self.random_date(), self.random_stuff(),
+                                       self.random_staff())
 
 
 def main(file_name):
-    resourse = input('resourses:').split(',')
+    resourse = sys.argv[1].split(',')
+    print(sys.argv[1])
     act = Randomizer(resourse)
     i = 0
-    old_data = headers
-    while i < 10:
+    old_data = ','.join(headers)
+    while i < 100:
         new_data = old_data + act.all_in()
         old_data = new_data + '\n'
         i += 1
@@ -43,4 +64,4 @@ def main(file_name):
 
 
 if __name__ == '__main__':
-    main("input.txt")
+    main(r"input.txt")
